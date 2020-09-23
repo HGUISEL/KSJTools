0326db590f38127fb03649fbee44c93de46d2230
kksj216 <21900044@handong.edu>
2020-09-24 04:41:52 +0900

    create printHelp method

--- src/main/java/edu/handong/csee/isel/Main.java
+++ src/main/java/edu/handong/csee/isel/Main.java
@@ -1,10 +1,19 @@
 package edu.handong.csee.isel;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 public class Main {
 
+	String originalFilePath;
+	String resultFilePath;
+	String posLabelName;
+	boolean help = false;
+	
 	public static void main (String[] args) {
         
 		Main  runner = new Main();
@@ -33,6 +42,10 @@ public class Main {
 	void runner (String[] args) {
 		
 		Options options = createOptions();
+		
+		if (parseOptions(options, args)) {
+			
+		}
 		
 		
 	}
@@ -70,4 +83,31 @@ public class Main {
 		
 	}
 	
+	boolean parseOptions(Options options, String[] args) {
+		
+		CommandLineParser parser = new DefaultParser();
+		
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			originalFilePath = cmd.getOptionValue("f");
+			resultFilePath = cmd.getOptionValue("r");
+			posLabelName = cmd.getOptionValue("p");
+			help = cmd.hasOption("h");
+			
+		} catch (Exception e) {
+			printHelp(options);
+			return false;
+		}
+		
+		return true;
+	}
+
+	private void printHelp(Options options) {
+		
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "This is tool for compare the defect result of spectral classifier with original arff file, so that we can get a defect prediction performace of spectral classifier.";
+		String footer = "Please check help page for input arguments.";
+		formatter.printHelp("./KSJtools", header, options, footer, true);
+		
+	}
 }
